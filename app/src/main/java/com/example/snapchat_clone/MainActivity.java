25053ba33c7a4bbc25076895b6baea803b58ac1f
wongnic <Nicole.Wong@canada.ca>
2019-01-29 09:18:13 -0500

    homescreen layout + firebase added

--- app/src/main/java/com/example/snapchat_clone/MainActivity.java
+++ app/src/main/java/com/example/snapchat_clone/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.snapchat_clone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /*
+        Enables fullscreen mode in the app
+        - hides the notification bar and navigation bar
+        - will show again if you swipe down from the top
+     */
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            );
+        }
     }
 }
