873d3e92107019fa3254d61a9544e66a4ba862c2
wongnic <Nicole.Wong@canada.ca>
2019-02-01 10:13:32 -0500

    updated imports

--- app/src/main/java/com/example/snapchat_clone/UserActivity.java
+++ app/src/main/java/com/example/snapchat_clone/UserActivity.java
@@ -2,6 +2,7 @@ package com.example.snapchat_clone;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -9,7 +10,6 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
