244e0bde3f1ce1575f52581540b89002908ec57c
Timmy Truong <timmy.truong@canada.ca>
2019-02-07 11:44:11 -0500

    Fully functional chat! FINALLY!

--- app/src/main/java/com/example/snapchat_clone/ChatActivity.java
+++ app/src/main/java/com/example/snapchat_clone/ChatActivity.java
@@ -1,31 +1,35 @@
 package com.example.snapchat_clone;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.content.Intent;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-
-public class ChatActivity extends AppCompatActivity implements View.OnClickListener{
+public class ChatActivity extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener {
 
 	// UI Declarations
 	TextView titleTextView;
@@ -33,6 +37,7 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
 	ListView messageListView;
 	EditText messageEditText;
 	Button sendButton;
+	ConstraintLayout constraint;
 
 	// Firebase Declarations
 	FirebaseAuth mAuth = FirebaseAuth.getInstance();
@@ -40,65 +45,25 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
 	DatabaseReference mRootRef = database.getReference();
 
 	// MISC Declarations
-	String message, uniqueKey,currentUserUID, clickedUserDisplay, currentUserDisplay, clickedUserUID;   // USER INFO
-	ArrayList<String> messages;                                                                         // Messages for the listView
-	ArrayAdapter<String> arrayAdapter;                                                                  // ArrayAdapter for the listView
-	boolean closing;                                                                                    // Switches only if the user presses back
-
+	String message, uniqueKey,currentUserUID, clickedUserDisplay, currentUserDisplay, clickedUserUID;   // USERS INFO & MESSAGE
+	ArrayList<String> messages, usernames;                                                              // Messages and usernames for the listView
+	SimpleAdapter adapter;                                                                              // SimpleAdapter for the listView
+	boolean closing, firstTime;                                                                         // Switches only if the user presses back
 
 	// When the "SEND" button is pressed, takes editText and sends it to the database
 	// Database - Stores into: Clicked User's Received & Current User's Sent
-	// Updates the text
 	public void sendMessage(View view) {
 		if (!messageEditText.getText().toString().isEmpty()) {
 			message = messageEditText.getText().toString();
 			uniqueKey = mRootRef.push().getKey();
-			// Clicked User's Store
-			mRootRef.child("Users").child(clickedUserDisplay).child("receivedMessages").child(currentUserUID).child(uniqueKey).setValue(message);
 			// Current User's Store
 			mRootRef.child("Users").child(currentUserDisplay).child("sentMessages").child(clickedUserUID).child(uniqueKey).setValue(message);
+			// Clicked User's Store
+			mRootRef.child("Users").child(clickedUserDisplay).child("receivedMessages").child(currentUserUID).child(uniqueKey).setValue(message);
 			messageEditText.setText("");
-			updateList();
 		} else {
 			Toast.makeText(this, "Please enter a message.", Toast.LENGTH_SHORT).show();
 		}
-	}
-
-	// Updates the listView onCreate and when a message is sent
-	// OnCreate - Takes all received messages at the user and displays
-	// sendMessage - Takes all received messages at the user AND the one(s) that were just sent then displays them
-	public void updateList() {
-		// Clears the arrayList before anything else
-		messages.clear();
-
-		// QUERY TO GET ALL MESSAGES
-		Query messagesQuery =  mRootRef.child("Users").orderByChild("displayName").equalTo(clickedUserDisplay);
-		messagesQuery.addListenerForSingleValueEvent(new ValueEventListener() {
-			@Override
-			public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-				if (dataSnapshot.exists()) {
-					// ALL RECEIVED MESSAGES FIRST (Users/clickedUser/sentMessages/currentUID/messages)
-					for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-						for (DataSnapshot children : snapshot.child("sentMessages").child(currentUserUID).getChildren()) {
-							messages.add(children.getValue(String.class));
-						}
-					}
-
-					// UPDATES THE LISTVIEW
-					arrayAdapter = new ArrayAdapter<>(ChatActivity.this, android.R.layout.simple_list_item_1, messages);
-					messageListView.setAdapter(arrayAdapter);
-
-					// GETS THE MESSAGES THAT WERE JUST SENT (Users/clickedUser/receivedMessages/currentUID/messages)
-					for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-						for (DataSnapshot children : snapshot.child("receivedMessages").child(currentUserUID).getChildren()) {
-							messages.add(children.getValue(String.class));
-						}
-					}
-				}
-			}
-			@Override
-			public void onCancelled(@NonNull DatabaseError databaseError) { }
-		});
 	}
 
 	// RUNS ONLY WHEN THE BACK BUTTON IS PRESSED
@@ -108,10 +73,18 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
 	//      Users/currentUser/receivedMessages/clickedUID/messages
 	@Override
 	public void onClick(View v) {
+
+		// hides keyboard if the listview is clicked
+		if (v.getId() == messageListView.getId() || v.getId() == titleTextView.getId() || v.getId() == constraint.getId()) {
+			InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+			inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+		}
+
 		switch (v.getId()) {
 			case R.id.backImageView:
 				closing = true;
 				messages.clear();
+				usernames.clear();
 
 				// Deletes ClickedOn Sent (Users/clickedUser/sentMessages/currentUID/messages)
 				Query deleteSenderQuery = mRootRef.child("Users").orderByChild("displayName").equalTo(clickedUserDisplay);
@@ -149,11 +122,9 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
 							}
 						}
 					}
-
 					@Override
 					public void onCancelled(@NonNull DatabaseError databaseError) {}
 				});
-
 				// GOES BACK TO USER ACTIVITY
 				finish();
 				break;
@@ -174,7 +145,7 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
 		clickedUserDisplay = intent.getStringExtra("userClicked");
 		currentUserDisplay = UserActivity.username;
 
-		// Defining UI elmeents
+		// Defining UI elements
 		messageEditText = findViewById(R.id.messageEditText);
 		messageListView = findViewById(R.id.messageListView);
 		sendButton = findViewById(R.id.sendButton);
@@ -182,14 +153,17 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
 		backImageView.setOnClickListener(this);
 		titleTextView = findViewById(R.id.titleTextView1);
 		titleTextView.setText(clickedUserDisplay);
+		titleTextView.setOnClickListener(this);
+		messageListView.setOnClickListener(this);
+		constraint = findViewById(R.id.layout);
 
 		// MISC Definitions
-		messages = new ArrayList<>();                                                                       // Initialize arraylist
-		arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, messages);     // Initialize arrayAdapter
-		closing = false;                                                                                    // Default the close to false
+		messages = new ArrayList<>();                               // Initialize arraylist
+		usernames = new ArrayList<>();                              // Initialize arraylist
+		closing = false;                                            // Default the close to false
+		firstTime = true;                                           // Default to true everytime the activity is opened
 
 		// QUERY to get the clicked on user's UID
-		// UPDATES THE LISTVIEW INITALLY
 		Query receiverUIDQuery = mRootRef.child("Users").orderByChild("displayName").equalTo(clickedUserDisplay);
 		receiverUIDQuery.addListenerForSingleValueEvent(new ValueEventListener() {
 			@Override
@@ -197,9 +171,62 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
 				if (dataSnapshot.exists()) {
 					for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
 						clickedUserUID = snapshot.child("uid").getValue(String.class);
+						// UpdateListInitial() - Line: 242
+						updateListInitial();
 					}
-					updateList();
 				}
+
+				/*
+				 * The following references and childEventListeners are in this method because they require clickedUserUID to run
+				 * Returns null if outside of this method - don't know why but dont remove these two references!
+				*/
+
+				// Reference to check if there has been any additions to Users/CurrentDisp/ReceivedMessages/ClickedUID
+				// Checks for any received messages from the clicked user and displays it
+				mRootRef.child("Users").child(currentUserDisplay).child("receivedMessages").child(clickedUserUID).addChildEventListener(new ChildEventListener() {
+					@Override
+					public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+						if (!firstTime) {
+							messages.add(dataSnapshot.getValue(String.class));
+							usernames.add(clickedUserDisplay);
+							// Update() - Line: 269
+							update();
+						}
+					}
+					@Override
+					public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {}
+					@Override
+					public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {}
+					@Override
+					public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {}
+					@Override
+					public void onCancelled(@NonNull DatabaseError databaseError) {}
+				});
+
+				// Reference to check if there has been any additions to Users/CurrentDisp/SentMessages/ClickedUID
+				// Checks if the user sent any messages to the clicked user and displays it
+				mRootRef.child("Users").child(currentUserDisplay).child("sentMessages").child(clickedUserUID).addChildEventListener(new ChildEventListener() {
+					@Override
+					public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+						if (!firstTime) {
+							messages.add(dataSnapshot.getValue(String.class));
+							usernames.add(currentUserDisplay);
+							// Update() - Line: 269
+							update();
+						}
+					}
+					@Override
+					public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {}
+					@Override
+					public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {}
+					@Override
+					public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {}
+					@Override
+					public void onCancelled(@NonNull DatabaseError databaseError) { }
+				});
+
+				// Changes after the first run to be able to run the updates
+				firstTime = false;
 			}
 			@Override
 			public void onCancelled(@NonNull DatabaseError databaseError) { }
@@ -210,6 +237,57 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
 		inputMethodManager.showSoftInput(messageEditText, 0);
 	}
 
+	// Updates the listView and searches the database for any messages the user has not seen
+	// Also displays the last sent messages to the clicked user if the clicked user is still viewing the message - for context
+	public void updateListInitial() {
+		// Clears the arraylists
+		messages.clear();
+		usernames.clear();
+		// QUERY TO GET ALL INITIAL CLICKED PAST MESSAGES
+		Query messagesQuery = mRootRef.child("Users").orderByChild("displayName").equalTo(currentUserDisplay);
+		messagesQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+			@Override
+			public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+				if (dataSnapshot.exists() && firstTime) {
+					// ALL RECEIVED MESSAGES FIRST (Users/clickedUser/sentMessages/currentUID/messages)
+					for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+						for (DataSnapshot children : snapshot.child("receivedMessages").child(clickedUserUID).getChildren()) {
+							messages.add(children.getValue(String.class));
+							usernames.add(clickedUserDisplay);
+						}
+					}
+					// Update() - Line: 269
+					update();
+				}
+			}
+			@Override
+			public void onCancelled(@NonNull DatabaseError databaseError) {} });
+	}
+
+	// Puts together the map to be displayed in the simpleAdapter
+	// Arranges the adapter and sets it to the listview
+	public void update() {
+
+		// Keys
+		String[] messageInfo = new String[]{"message", "username"};
+		int[] messageListInfo = new int[]{R.id.message, R.id.username};
+
+		// List of Maps to store the messages
+		List<HashMap<String, String>> fillMaps = new ArrayList<>();
+
+		// Filling the map
+		for (int i = 0; i < messages.size(); i++) {
+			HashMap<String, String> map = new HashMap<>();
+			map.put("message", messages.get(i));
+			map.put("username", usernames.get(i));
+			Log.i("map", map.toString());
+			fillMaps.add(map);
+		}
+
+		// Setting the adapters
+		adapter = new SimpleAdapter(ChatActivity.this, fillMaps, R.layout.message_list_view_item, messageInfo, messageListInfo);
+		messageListView.setAdapter(adapter);
+	}
 
 	/*
   Enables fullscreen mode in the app
@@ -229,5 +307,20 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
 							| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
 			);
 		}
+	}
+
+	// hides the keyboard if enter is clicked
+	@Override
+	public boolean onKey(View view, int i, KeyEvent keyEvent) {
+		if (i == KeyEvent.KEYCODE_ENTER && keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
+			keyBoardDown(view);
+		}
+		return false;
+	}
+
+	// Keyboard down method
+	public void keyBoardDown(View view) {
+		InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+		inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
 	}
 }
